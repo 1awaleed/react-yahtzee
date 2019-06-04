c1d9b9e8aea8cb2c4ff58df578f8c8f67a82fa10
Ahmed Waleed <1ahmedwaleed@gmail.com>
2019-06-04 22:12:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Yahtzee Dice Game</h1>
+<p>A react game built of several compnents</p>
+
+<ul>
+    <li>Game component</li>
+    <li>Dice Component</li>
+    <li>Die component</li>
+    <li>Score Table component</li>
+    <li>Rule row component</li>
+</ul>
+<p>The game is fully functional but can have better css styling.</p>
+<img src="./yahtzee.png">
---
+++ src/RuleRow.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './RuleRow.css';
+
+class RuleRow extends Component {
+	render () {
+		const { score, name, doScore, description } = this.props;
+		const isDisabled = score !== undefined;
+		return (
+			<tr
+				className={`RuleRow RuleRow-${isDisabled ? 'disabled' : 'active'}`}
+				onClick={isDisabled ? null : doScore}
+			>
+				<td className='RuleRow-name'>{name}</td>
+				<td className='RuleRow-score'>{isDisabled ? score : description}</td>
+			</tr>
+		);
+	}
+}
+
+export default RuleRow;
